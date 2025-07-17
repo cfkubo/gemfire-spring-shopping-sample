2aaccd4f7e224b03e6ce2c48e263d6536b92b6eb
cfkubo <arul.vannala@gmail.com>
2025-07-16 18:12:56 -0600

    cleanup

--- src/main/java/com/example/config/GemFireConfig.java
+++ src/main/java/com/example/config/GemFireConfig.java
@@ -13,9 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.net.InetSocketAddress;
-import java.util.Collections;
-
 @Configuration
 public class GemFireConfig {
 
